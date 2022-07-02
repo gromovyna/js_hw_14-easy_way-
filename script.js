f874c0c486e14ab6c4d0ccf3bd8692e43da2dd99
Влад <vlad@MacBook-Pro-Vlad.local>
2022-07-02 15:10:19 +0300

    add simple cod

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let obj = { 
+    x: 10, 
+    y: 20, 
+    inner: { 
+        x: 20,  
+        z: 30
+    },
+    foo2: { 
+        k: 23, 
+        p: 13 
+    } 
+}
+
+let newObj = {
+    ...obj,...obj.inner,...obj.foo2
+}
+console.log(newObj);
